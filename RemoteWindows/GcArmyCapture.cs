47e6cf6785db3efda598d7b431b2641252eb5400
nt153133 <52430037+nt153133@users.noreply.github.com>
2019-12-07 17:36:53 -0500

    Added squadron windows

--- RemoteWindows/GcArmyCapture.cs
+++ RemoteWindows/GcArmyCapture.cs
@@ -14,6 +14,10 @@
             SendAction(1, 3, 0xd);
         }
 
+        /// <summary>
+        ///     Sets the squadron command mission
+        /// </summary>
+        /// <param name="index">The duty index from the list starting at 0.</param>
         public void SelectDuty(int index)
         {
             SendAction(2, 3, 0xB, 4, (ulong) index);
