ac5a095239e3e1e3ce07e09d3545d9207d0db058
nt153133 <52430037+nt153133@users.noreply.github.com>
2019-12-08 11:29:30 -0500

    ignore old deepdive offsets

--- Memory/Offsets.cs
+++ Memory/Offsets.cs
@@ -31,17 +31,17 @@ Orginal work done by zzi, contibutions by Omninewb, Freiheit, and mastahg
         [Offset("Search 48 89 5C 24 ? 57 48 83 EC ? 88 51 ? 49 8B F9")] 
         internal static IntPtr RepairWindowOpen;
 
-        [Offset("Search 0F B7 82 ? ? ? ? 2B C8 48 8D 0C 89 Add 3 Read32")] // 0x332
-        internal static int WallStartingPoint;
+    //    [Offset("Search 0F B7 82 ? ? ? ? 2B C8 48 8D 0C 89 Add 3 Read32")] // 0x332
+    //    internal static int WallStartingPoint;
 
-        [Offset("Search 49 8D BE ? ? ? ? 0F 1F 44 00 ? 8B 17 Add 3 Read32")] //0x140
-        internal static int Starting;
+     //   [Offset("Search 49 8D BE ? ? ? ? 0F 1F 44 00 ? 8B 17 Add 3 Read32")] //0x140
+     //   internal static int Starting;
 
-        [Offset("Search 41 8B 96 ? ? ? ? 84 C9 Add 3 Read32")] //0x13c
-        internal static int UNK_StartingCircle;
+      //  [Offset("Search 41 8B 96 ? ? ? ? 84 C9 Add 3 Read32")] //0x13c
+       // internal static int UNK_StartingCircle;
 
-        [Offset("Search 42 0F B6 8C 28 ? ? ? ? 84 0E Add 5 Read32")] //0x1860
-        internal static int WallGroupEnabled;
+        //[Offset("Search 42 0F B6 8C 28 ? ? ? ? 84 0E Add 5 Read32")] //0x1860
+        //internal static int WallGroupEnabled;
     }
 #pragma warning restore CS0649
 }
