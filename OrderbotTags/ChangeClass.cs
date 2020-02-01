f32aca640a17f7cb2e19ed8c2a7e82b8e90f77a9
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-02-01 16:38:34 -0500

    ChangeJob(Add Gearset)

--- OrderbotTags/ChangeClass.cs
+++ OrderbotTags/ChangeClass.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Buddy.Coroutines;
 using Clio.XmlEngine;
 using ff14bot.Enums;
+using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.NeoProfiles;
 using TreeSharp;
 
-namespace ff14bot.NeoProfiles
+namespace ff14bot
 {
     [XmlElement("ChangeClass")]
     public class ChangeClass : ProfileBehavior
@@ -38,14 +40,46 @@ namespace ff14bot.NeoProfiles
         private async Task<bool> ChangeJob()
         {
             var gearSets = GearsetManager.GearSets.Where(i => i.InUse);
-
+            Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Started");
             ClassJobType newjob;
             var foundJob = Enum.TryParse(job.Trim(), true, out newjob);
-
+            Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found job: {foundJob} Job:{newjob}");
             if (foundJob && gearSets.Any(gs => gs.Class == newjob))
             {
+                Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found GearSet");
                 gearSets.First(gs => gs.Class == newjob).Activate();
                 await Coroutine.Sleep(1000);
+            }
+            
+            else if (foundJob)
+            {
+                job = job.Trim() + ("s_Primary_Tool");
+
+                ItemUiCategory category;
+                var categoryFound = Enum.TryParse(job, true, out category);
+                
+                if (categoryFound)
+                {
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found Item Category: {categoryFound} Category:{category}");
+                    var item = InventoryManager.FilledInventoryAndArmory.Where(i => i.Item.EquipmentCatagory == category).OrderByDescending(i=> i.Item.ItemLevel).FirstOrDefault();
+                    BagSlot EquipSlot = InventoryManager.GetBagByInventoryBagId(InventoryBagId.EquippedItems)[EquipmentSlot.MainHand];
+                    
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found Item {item}");
+                    if (item != null)
+                    {
+                        item.Move(EquipSlot);
+                    }
+                    
+                    await Coroutine.Sleep(1000);
+                    
+                    ChatManager.SendChat("/gs save");
+                    
+                    await Coroutine.Sleep(1000);
+                }
+                else
+                {
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Couldn't find item category'");
+                }
             }
 
             _isDone = true;
