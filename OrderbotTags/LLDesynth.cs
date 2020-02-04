601562e2e4a9b2aa269252c9ea1cc100b4db1360
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-02-04 15:05:48 -0500

    LLDesynth tag added

---
+++ OrderbotTags/LLDesynth.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot.Managers;
+using ff14bot.NeoProfiles;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.Extensions;
+using TreeSharp;
+
+namespace LlamaLibrary.OrderbotTags
+{
+    [XmlElement("LLDesynth")]
+    public class LLDesynth: ProfileBehavior
+    {
+        private bool _isDone;
+
+        [XmlAttribute("ItemIds")] 
+        public int[] ItemIds { get; set; }
+        
+        [DefaultValue(500)]
+        [XmlAttribute("DesynthDelay")]
+        public int DesynthDelay { get; set; }
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => DesynthItems(ItemIds));
+        }
+
+        private async Task DesynthItems(int[] itemId)
+        {
+            var itemsToDesynth = InventoryManager.FilledSlots.Where(bs => bs.IsDesynthesizable && itemId.Contains((int) bs.RawItemId));
+
+            //Log($"{itemsToDesynth.Count()}");
+            
+            foreach (var item in itemsToDesynth)
+            {
+                Log($"Desynthesize Item - Name: {item.Item.CurrentLocaleName}");
+
+                item.Desynth();
+                
+                await Coroutine.Wait(10000, () => SalvageResult.IsOpen);
+
+                if (SalvageResult.IsOpen)
+                {
+                    SalvageResult.Close();
+                    await Coroutine.Wait(5000, () => !SalvageResult.IsOpen);
+                }
+                else
+                {
+                    Log("Result didn't open");
+                    break;
+                }
+
+                await Coroutine.Sleep(DesynthDelay);
+            }
+
+            _isDone = true;
+        }
+    }
+}
