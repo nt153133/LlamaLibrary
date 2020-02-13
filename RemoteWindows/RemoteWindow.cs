c00584f4e648625c521e3d548c2d8bbeaedc97c6
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-02-13 00:06:05 -0500

    Cleanup

--- RemoteWindows/RemoteWindow.cs
+++ RemoteWindows/RemoteWindow.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using ff14bot;
 using ff14bot.Managers;
-using ff14bot.NeoProfiles;
 using ff14bot.RemoteWindows;
 
 namespace LlamaLibrary.RemoteWindows
@@ -14,50 +12,50 @@ namespace LlamaLibrary.RemoteWindows
 
         protected RemoteWindow(string name) : base(name)
         {
-           // _instance = new T();
+            // _instance = new T();
         }
     }
-    
+
     public abstract class RemoteWindow
     {
         internal string _name;
         private const int Offset0 = 0x1CA;
         private const int Offset2 = 0x160;
-/*      Can Also do this: Will pull the same offsets Mastahg stores in RB
-        var off = typeof(Core).GetProperty("Offsets", BindingFlags.NonPublic | BindingFlags.Static);
-        var struct158 = off.PropertyType.GetFields()[72];
-        var offset0 = (int)struct158.FieldType.GetFields()[0].GetValue(struct158.GetValue(off.GetValue(null)));
-        var offset2 = (int)struct158.FieldType.GetFields()[2].GetValue(struct158.GetValue(off.GetValue(null)));
-*/
+        /*      Can Also do this: Will pull the same offsets Mastahg stores in RB
+                var off = typeof(Core).GetProperty("Offsets", BindingFlags.NonPublic | BindingFlags.Static);
+                var struct158 = off.PropertyType.GetFields()[72];
+                var offset0 = (int)struct158.FieldType.GetFields()[0].GetValue(struct158.GetValue(off.GetValue(null)));
+                var offset2 = (int)struct158.FieldType.GetFields()[2].GetValue(struct158.GetValue(off.GetValue(null)));
+        */
 
         public virtual bool IsOpen => WindowByName != null;
 
         public virtual string Name => _name;
 
         internal AtkAddonControl WindowByName => RaptureAtkUnitManager.GetWindowByName(Name);
-        
+
         protected bool HasAgentInterfaceId => GetAgentInterfaceId() != 0;
 
         protected RemoteWindow(string name)
         {
             _name = name;
         }
-        
+
         public virtual void Close()
         {
             SendAction(1, 3uL, 4294967295uL);
         }
-        
+
         public int GetAgentInterfaceId()
         {
             if (WindowByName == null)
                 return 0;
-            
+
             var test = WindowByName.TryFindAgentInterface();
-            
+
             return test == null ? 0 : test.Id;
         }
-        
+
         protected TwoInt[] ___Elements()
         {
             if (WindowByName == null) return null;
@@ -68,7 +66,7 @@ namespace LlamaLibrary.RemoteWindows
 
         protected ushort ElementCount()
         {
-            return WindowByName != null ? Core.Memory.Read<ushort>(WindowByName.Pointer + Offset0) : (ushort) 0;
+            return WindowByName != null ? Core.Memory.Read<ushort>(WindowByName.Pointer + Offset0) : (ushort)0;
         }
 
         protected void SendAction(int pairCount, params ulong[] param)
