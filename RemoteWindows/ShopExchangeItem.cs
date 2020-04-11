d9cce8dcb323b9fce3f131afa0236c9554ae7a25
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-04-11 15:10:22 -0400

    Shop exchange fix

--- RemoteWindows/ShopExchangeItem.cs
+++ RemoteWindows/ShopExchangeItem.cs
@@ -1,13 +1,16 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Buddy.Coroutines;
+using ff14bot.Behavior;
+using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.RemoteWindows;
 using LlamaLibrary.Structs;
 
 namespace LlamaLibrary.RemoteWindows
 {
-    public class ShopExchangeItem: RemoteWindow<ShopExchangeItem>
+    public class ShopExchangeItem : RemoteWindow<ShopExchangeItem>
     {
         private const string WindowName = "ShopExchangeItem";
 
@@ -15,7 +18,7 @@ namespace LlamaLibrary.RemoteWindows
         {
             _name = WindowName;
         }
-        
+
         public async Task<uint> Purchase(uint itemId, uint itemCount = 1)
         {
             if (!IsOpen) return 0u;
@@ -23,14 +26,14 @@ namespace LlamaLibrary.RemoteWindows
             var items = SpecialShopManager.Items;
 
             var specialShopItem = items?.Cast<SpecialShopItem?>().FirstOrDefault(i => i.HasValue && i.Value.ItemIds.Contains(itemId));
-
+            //Logging.Write(Colors.Fuchsia, $"[ShopExchangeItem] Buying {specialShopItem}");
             if (!specialShopItem.HasValue) return 0u;
 
             if (itemCount > specialShopItem.Value.Item0.StackSize) itemCount = specialShopItem.Value.Item0.StackSize;
 
             if (!CanAfford(specialShopItem.Value))
                 return 0;
-            
+            // Logging.Write(Colors.Fuchsia, $"[Purchase] Can afford {CanAfford(specialShopItem.Value)}");
             var index = items.IndexOf(specialShopItem.Value);
             var obj = new ulong[8]
             {
@@ -46,45 +49,52 @@ namespace LlamaLibrary.RemoteWindows
             obj[3] = (uint) index;
             obj[5] = itemCount;
             SendAction(4, obj);
-
+            // Logging.Write(Colors.Fuchsia, $"[Purchase] Sent Action");
             await Coroutine.Wait(5000, () => RaptureAtkUnitManager.GetWindowByName("ShopExchangeItemDialog") != null);
 
             if (RaptureAtkUnitManager.GetWindowByName("ShopExchangeItemDialog") != null)
             {
-                RaptureAtkUnitManager.GetWindowByName("ShopExchangeItemDialog").SendAction(1,3,0);
+                //Logging.Write(Colors.Fuchsia, $"[Purchase] ShopExchangeItemDialog Open");
+                RaptureAtkUnitManager.GetWindowByName("ShopExchangeItemDialog").SendAction(1, 3, 0);
                 await Coroutine.Wait(5000, () => RaptureAtkUnitManager.GetWindowByName("ShopExchangeItemDialog") == null);
-                
-                await Coroutine.Wait(2000, () => SelectYesno.IsOpen);
+
+                await Coroutine.Wait(2000, () => SelectYesno.IsOpen || Request.IsOpen);
 
                 if (SelectYesno.IsOpen)
                 {
+                    //Logging.Write(Colors.Fuchsia, $"[Purchase] Yes/no");
                     SelectYesno.Yes();
                     await Coroutine.Wait(2000, () => !SelectYesno.IsOpen);
-                    
-                    await Coroutine.Wait(2000, () => Request.IsOpen);
 
-                    if (Request.IsOpen)
+                    await Coroutine.Wait(3000, () => Request.IsOpen);
+                }
+
+                if (Request.IsOpen)
+                {
+                    // Logging.Write(Colors.Fuchsia, $"[Purchase] Request");
+                    for (int i = 0; i < 3; i++)
                     {
-                        for (int i = 0; i < 3; i++)
+                        var item = InventoryManager.FilledInventoryAndArmory.FirstOrDefault(j => j.RawItemId == specialShopItem.Value.CurrencyTypes[i] && j.Count >= specialShopItem.Value.CurrencyCosts[i]);
+                        // Logging.Write(Colors.Fuchsia, $"[Purchase] Request item {item}");
+                        if (item != null)
                         {
-                            var item = InventoryManager.FilledInventoryAndArmory.FirstOrDefault(j => j.RawItemId == specialShopItem.Value.CurrencyTypes[i] && j.Count >= specialShopItem.Value.CurrencyCosts[i]);
-
-                            if (item != null)
-                            {
-                                item.Handover();
-                                await Coroutine.Sleep(500);
-                            }
+                            item.Handover();
+                            await Coroutine.Sleep(500);
                         }
-                        if (Request.HandOverButtonClickable)
-                            Request.HandOver();
-                        
-                        await Coroutine.Sleep(500);
                     }
+
+                    if (Request.HandOverButtonClickable)
+                        Request.HandOver();
+
+                    await Coroutine.Sleep(500);
+                }
+                else
+                {
+                    //Logging.Write(Colors.Fuchsia, $"[Purchase] Request Not open");
                 }
             }
-            
-            
-            
+
+
             return itemCount;
         }
 
@@ -97,9 +107,11 @@ namespace LlamaLibrary.RemoteWindows
                     continue;
                 }
 
+
                 if (!InventoryManager.FilledInventoryAndArmory.Any(j => j.RawItemId == item.CurrencyTypes[i] && j.Count >= item.CurrencyCosts[i]))
                     return false;
             }
+
             return true;
         }
     }
