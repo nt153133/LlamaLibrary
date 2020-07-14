186af2d31320cffbf5d869222d51312d995c3da4
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-07-14 14:12:49 -0400

    BuyBluTotem tag and Achievement helper

---
+++ OrderbotTags/BuyBluTotem.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot;
+using ff14bot.Managers;
+using ff14bot.NeoProfiles;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.Helpers;
+using LlamaLibrary.RemoteWindows;
+using TreeSharp;
+using Character = ff14bot.Objects.Character;
+using static ff14bot.RemoteWindows.Talk;
+
+namespace LlamaLibrary.OrderbotTags
+{
+[XmlElement("BuyBluTotem")]
+    public class BuyBluTotem : ProfileBehavior
+    {
+        private bool _isDone;
+        private bool _isOpening;
+
+        public override bool IsDone => _isDone;
+
+        [XmlAttribute("NpcId")]
+        public int NpcId { get; set; }
+
+        [XmlAttribute("ItemId")]
+        public int ItemId { get; set; }
+
+        [XmlAttribute("SelectString")]
+        public int selectString { get; set; }
+
+
+        public override bool HighPriority => true;
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => BuyItem(ItemId, NpcId,  selectString));
+        }
+
+        private async Task BuyItem(int itemId, int npcId, int selectString)
+        {
+            var unit = GameObjectManager.GetObjectsByNPCId<Character>((uint) npcId).OrderBy(r => r.Distance()).FirstOrDefault();
+
+            if (unit == null)
+            {
+                _isDone = true;
+                return;
+            }
+
+            if (!FreeShop.Instance.IsOpen && unit.Location.Distance(Core.Me.Location) > 4f)
+            {
+                await Navigation.OffMeshMove(unit.Location);
+                await Coroutine.Sleep(500);
+            }
+
+            unit.Interact();
+
+            await Coroutine.Wait(5000, () => Conversation.IsOpen);
+
+            if (Conversation.IsOpen)
+            {
+                Conversation.SelectLine((uint) selectString);
+                
+                await Coroutine.Wait(5000, () => DialogOpen || FreeShop.Instance.IsOpen);
+
+                if (DialogOpen) Next();
+                
+                await Coroutine.Wait(5000, () => FreeShop.Instance.IsOpen);
+                
+                if (FreeShop.Instance.IsOpen)
+                {
+                    //Log("Opened");
+                    await FreeShop.Instance.BuyItem((uint) itemId);
+                }
+
+                await Coroutine.Wait(2000, () => FreeShop.Instance.IsOpen);
+                if (FreeShop.Instance.IsOpen)
+                {
+                    FreeShop.Instance.Close();
+                }
+            }
+
+            _isDone = true;
+        }
+    }
+}
---
+++ RemoteWindows/FreeShop.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using ff14bot.Managers;
+using ff14bot.RemoteWindows;
+
+namespace LlamaLibrary.RemoteWindows
+{
+    public class FreeShop: RemoteWindow<FreeShop>
+    {
+        private const string WindowName = "FreeShop";
+
+        public FreeShop() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+        
+        public int NumberOfItems =>  ___Elements()[3].TrimmedData;
+        
+        public List<Item> GetAvailItems()
+        {
+            var currentElements = ___Elements();
+
+            var itemElements = new ArraySegment<TwoInt>(currentElements, 65, NumberOfItems);
+
+            return itemElements.Select(item => DataManager.GetItem((uint) (item.TrimmedData))).ToList();
+        }
+
+        public async Task<bool> BuyItem(uint itemId)
+        {
+            if (InventoryManager.FilledSlots.Any(i => i.RawItemId == itemId))
+                return true;
+
+            if (IsOpen)
+            {
+                ClickItem(GetItemIndex(itemId));
+                await Coroutine.Wait(5000, () => InventoryManager.FilledSlots.Any(i => i.RawItemId == itemId));
+            }
+
+            return (InventoryManager.FilledSlots.Any(i => i.RawItemId == itemId));
+        }
+
+        public int GetItemIndex(uint itemId)
+        {
+            var items = GetAvailItems().ToArray();
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Id == itemId)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void ClickItem(int index)
+        {
+            if (index >=0)
+                SendAction(2,3,0,3,(ulong) index);
+        }
+    }
+}
