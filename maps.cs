78aea7af1b418f2d7237085ab3664962cba3e096
Senior-Moon-Moon <45137257+Senior-Moon-Moon@users.noreply.github.com>
2020-09-20 14:09:49 -0500

    Create maps.cs
    does stuff with treasure maps. 
    
    Could probably be expanded some in the future.

---
+++ maps.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ff14bot;
+using ff14bot.Enums;
+using ff14bot.Managers;
+using LlamaLibrary.Memory;
+using LlamaLibrary.Memory.Attributes;
+
+namespace LlamaLibrary
+{
+    public class TreasureMap
+    {
+        static TreasureMap()
+        {
+            var patternFinder = new GreyMagic.PatternFinder(Core.Memory);
+            CurrentMap = patternFinder.Find("Search 48 8d 0d ?? ?? ?? ?? e8 ?? ?? ?? ?? 48 8d 0d ?? ?? ?? ?? 0f b7 d8 e8 ?? ?? ?? ?? Add 3 TraceRelative");
+
+        }
+
+        /// <summary>
+        /// Pointer to where the game stores the map subkey
+        /// </summary>
+        private static IntPtr CurrentMap;
+        /// <summary>
+        /// Provides a KeyItem => TreasureHuntRank lookup because the client does the same thing.
+        /// </summary>
+        private static Dictionary<uint, short> MapPrimary = new Dictionary<uint, short>
+        {
+            
+            //KeyEventItemName
+            { 2001087, 1 },
+            { 2001088, 2 },
+            { 2001089, 3 },
+            { 2001090, 4 },
+            { 2001091, 5 },
+            { 2001223, 6 },
+            { 2001352, 7 },
+            { 2001551, 8 },
+            { 2001762, 9 },
+            { 2001763, 10 },
+            { 2001764, 11 },
+            { 2002209, 12 },
+            { 2002210, 13 },
+            { 2002386, 14 },
+            { 2002503, 16 },
+            { 2002663, 17 },
+            { 2002664, 18 },
+            
+            //InstanceMap
+            { 2001977, 11 },
+            { 2002236, 13 },
+            { 2002260, 14 },
+            { 2002504, 16 },
+            { 2002665, 18 },
+            
+        };
+
+        private static short PrimaryKey
+        {
+            get
+            {
+                var items = InventoryManager.GetBagByInventoryBagId(InventoryBagId.KeyItems).FilledSlots
+                    .FirstOrDefault(i => MapPrimary.ContainsKey(i.RawItemId));
+                if (items != null)
+                {
+                    return MapPrimary[items.RawItemId];
+                }
+
+                return 0;
+            }
+        }
+
+        public static short SecondaryKey => Core.Memory.Read<short>(CurrentMap);
+    }
+}
