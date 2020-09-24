5a55ac420a881dabe15f17e1293e4fe67399713c
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-09-23 21:59:35 -0400

    BuyGCItem tag

---
+++ OrderbotTags/BuyGCItem.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot.NeoProfiles;
+using TreeSharp;
+
+namespace LlamaLibrary.OrderbotTags
+{
+    public class BuyGCItem: ProfileBehavior
+    {
+
+            private bool _isDone;
+
+            [XmlAttribute("ItemId")] public int ItemId { get; set; }
+        
+            [XmlAttribute("Count")] 
+            [DefaultValue(1)]
+            public int Count { get; set; }
+
+            public override bool HighPriority => true;
+
+            public override bool IsDone => _isDone;
+
+            protected override void OnStart()
+            {
+            }
+
+            protected override void OnDone()
+            {
+            }
+
+            protected override void OnResetCachedDone()
+            {
+                _isDone = false;
+            }
+
+            protected override Composite CreateBehavior()
+            {
+                return new ActionRunCoroutine(r => BuyGCKnownItem(ItemId, Count));
+            }
+
+            private async Task BuyGCKnownItem(int itemId, int Count)
+            {
+                await LlamaLibrary.Helpers.GrandCompanyShop.BuyKnownItem((uint) itemId, Count);
+
+                _isDone = true;
+            }
+        }
+    
+}
