2549170fd7dbd252bcdfe4b85de6920035e9f19b
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-10-03 22:45:05 -0400

    It's tuck's fault

---
+++ ScriptConditions/Extras.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using ff14bot.Managers;
+using ff14bot.Objects;
+
+namespace LlamaLibrary.ScriptConditions
+{
+    public static class Extras
+    {
+        public static int NumAttackableEnemies(float dist = 0, params uint[] ids)
+        {
+            if (ids.Length == 0)
+            {
+                if (dist > 0) return GameObjectManager.GetObjectsOfType<BattleCharacter>().Count(i => i.CanAttack && i.IsTargetable && i.Distance() < dist);
+                else return GameObjectManager.GetObjectsOfType<BattleCharacter>().Count(i => i.CanAttack && i.IsTargetable);
+            }
+            else
+            {
+                if (dist > 0) return GameObjectManager.GetObjectsByNPCIds<BattleCharacter>(ids).Count(i => i.CanAttack && i.IsTargetable && i.Distance() < dist);
+                else return GameObjectManager.GetObjectsByNPCIds<BattleCharacter>(ids).Count(i => i.CanAttack && i.IsTargetable);
+            }
+        }
+    }
+}
