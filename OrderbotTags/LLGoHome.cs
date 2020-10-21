bbd6ee931966fd5373446c1721a34198f31ae5bd
domesticwarlord86 <57776315+domesticwarlord86@users.noreply.github.com>
2020-10-21 15:04:39 -0500

    Created OrderBot tag for LlamaLibrary's GoHome Task.

---
+++ OrderbotTags/LLGoHome.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot;
+using ff14bot.Behavior;
+using ff14bot.Helpers;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.NeoProfiles;
+using ff14bot.Objects;
+using TreeSharp;
+
+namespace LlamaLibrary.OrderbotTags
+{
+    [XmlElement("LLGoHome")]
+    public class LLGoHome : ProfileBehavior
+    {
+        private bool _isDone;
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => LLGoHomeTask());
+        }
+
+        private async Task LLGoHomeTask()
+        {
+            await LlamaLibrary.Helpers.GeneralFunctions.GoHome();	
+
+            _isDone = true;
+        }
+    }
+}
