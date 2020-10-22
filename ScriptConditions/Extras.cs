39ba620bd0ecf9e4ff669b33087a1db78a7cb895
Bread <TuckMeIntoBread@users.noreply.github.com>
2020-10-21 18:04:43 -1000

    Lowest iLVL ScriptConditions
    for use in Angles' profiles

--- ScriptConditions/Extras.cs
+++ ScriptConditions/Extras.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ff14bot.Enums;
 using ff14bot.Managers;
 using ff14bot.Objects;
+using LlamaLibrary.Extensions;
+using LlamaLibrary.Helpers;
 
 namespace LlamaLibrary.ScriptConditions
 {
@@ -19,10 +23,28 @@ namespace LlamaLibrary.ScriptConditions
                 else return GameObjectManager.GetObjectsByNPCIds<BattleCharacter>(ids).Count(i => i.CanAttack && i.IsTargetable);
             }
         }
-		
-		public static int SphereCompletion(int itemID)
-				{
-					return (int) (InventoryManager.FilledInventoryAndArmory.FirstOrDefault(i => i.RawItemId == (uint)itemID).SpiritBond);
-				}		
+
+        public static int SphereCompletion(int itemID)
+        {
+            return (int) (InventoryManager.FilledInventoryAndArmory.FirstOrDefault(i => i.RawItemId == (uint)itemID).SpiritBond);
+        }
+
+        public static int LowestCrafterILvl()
+        {
+            IEnumerable<GearSet> sets = GearsetManager.GearSets.Where(g => g.Class.IsDoh() && g.Gear.Any());
+            return sets.Any() ? sets.Min(GeneralFunctions.GetGearSetiLvl) : 0;
+        }
+
+        public static int LowestGathererILvl()
+        {
+            IEnumerable<GearSet> sets = GearsetManager.GearSets.Where(g => g.Class.IsDol() && g.Gear.Any());
+            return sets.Any() ? sets.Min(GeneralFunctions.GetGearSetiLvl) : 0;
+        }
+        
+        public static int LowestMINBTNILvl()
+        {
+            IEnumerable<GearSet> sets = GearsetManager.GearSets.Where(g => g.Class.IsDol() && g.Class != ClassJobType.Fisher && g.Gear.Any());
+            return sets.Any() ? sets.Min(GeneralFunctions.GetGearSetiLvl) : 0;
+        }
     }
 }
