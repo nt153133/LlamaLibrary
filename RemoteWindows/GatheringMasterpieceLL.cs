67fa6ccbb4bc72e0001ac14ac4123d5f76288d6c
nt153133 <52430037+nt153133@users.noreply.github.com>
2020-12-09 20:05:27 -0500

    Temp collectable gathering window

--- RemoteWindows/GatheringMasterpieceLL.cs
+++ RemoteWindows/GatheringMasterpieceLL.cs
@@ -35,6 +35,22 @@ namespace LlamaLibrary.RemoteWindows
             {
                 "IntuitionRate",
                 37
+            },
+            {
+                "Skill1",
+                30
+            },
+            {
+                "Skill2Estimate",
+                31
+            },
+            {
+                "Skill2Max",
+                32
+            },
+            {
+                "Skill3",
+                33
             }
         };
 
@@ -43,6 +59,10 @@ namespace LlamaLibrary.RemoteWindows
         public int Integrity => ___Elements()[Properties["Integrity"]].TrimmedData;
         public int MaxIntegrity => ___Elements()[Properties["MaxIntegrity"]].TrimmedData;
         public int ItemID => ___Elements()[Properties["ItemID"]].TrimmedData;
+        public int Scour => ___Elements()[Properties["Skill1"]].TrimmedData;
+        public int BrazenEstimate => ___Elements()[Properties["Skill2Estimate"]].TrimmedData;
+        public int Brazen2Max => ___Elements()[Properties["Skill2Max"]].TrimmedData;
+        public int Meticulous => ___Elements()[Properties["Skill3"]].TrimmedData;
         public int IntuitionRate => ___Elements()[Properties["IntuitionRate"]].TrimmedData;
 
         public void Collect()
@@ -50,6 +70,26 @@ namespace LlamaLibrary.RemoteWindows
             if (IsOpen)
                 SendAction(1,3,0);
         }
+
+        public void SetScrutiny(bool value = true)
+        {
+            if (value)
+                SendAction(3,3,0x65,0,0,2,1);
+            else
+            {
+                SendAction(3,3,0x65,0,0,2,0);
+            }
+        }
         
+        public void SetCollectorsIntuition(bool value = true)
+        {
+            if (value)
+                SendAction(3,3,0x66,0,0,2,1);
+            else
+            {
+                SendAction(3,3,0x66,0,0,2,0);
+            }
+        }
+
     }
 }
