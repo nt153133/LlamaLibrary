54f62ee0c8893c02553fe854937e2fa3d293420a
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-03-02 15:35:26 -0500

    SatisfactionSupplyResult

---
+++ RemoteWindows/SatisfactionSupplyResult.cs
@@ -0,0 +1,17 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class SatisfactionSupplyResult: RemoteWindow<SatisfactionSupplyResult>
+    {
+        private const string WindowName = "SatisfactionSupplyResult";
+        
+        public SatisfactionSupplyResult() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        public void Confirm()
+        {
+            SendAction(1,3,0);
+        }
+    }
+}
