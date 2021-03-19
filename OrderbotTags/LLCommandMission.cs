c1dd8b3ea3bf64cba6bb36a8f88b30375ac3ae57
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-03-18 23:33:34 -0400

    Fix domestic's snafu

--- OrderbotTags/LLCommandMission.cs
+++ OrderbotTags/LLCommandMission.cs
@@ -55,9 +55,9 @@ namespace LlamaLibrary.OrderbotTags
             
                 //Inside Barracks
 
-                await Navigation.OffMeshMoveInteract(GameObjectManager.GetObjectByNPCId(GrandCompanyHelper.GetNpcByType(GCNpc.Squadron_Sergeant)));
+                //await Navigation.OffMeshMoveInteract(GameObjectManager.GetObjectByNPCId(GrandCompanyHelper.GetNpcByType(GCNpc.Squadron_Sergeant)));
                 
-                GameObjectManager.GetObjectByNPCId(GrandCompanyHelper.GetNpcByType(GCNpc.Squadron_Sergeant)).Interact();
+                //GameObjectManager.GetObjectByNPCId(GrandCompanyHelper.GetNpcByType(GCNpc.Squadron_Sergeant)).Interact();
                 
                 await Coroutine.Wait(10000, () => Talk.DialogOpen);
                 await Buddy.Coroutines.Coroutine.Sleep(500);
