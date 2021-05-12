c3f07a50dc9fe2f377fd93b9848bcb772c954845
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-05-12 01:21:15 -0400

    CN meld agent offset

--- RemoteAgents/AgentMeld.cs
+++ RemoteAgents/AgentMeld.cs
@@ -11,7 +11,7 @@ namespace LlamaLibrary.RemoteAgents
         private static class Offsets
         {
             [Offset("Search 48 8D 05 ? ? ? ? 33 FF 48 89 03 48 8D 4B ? Add 3 TraceRelative")]
-            [OffsetCN("Search 48 8D 05 ? ? ? ? 48 8D 4B ? 48 89 03 E8 ? ? ? ? 48 8D 4B ? E8 ? ? ? ? 33 C9 Add 3 TraceRelative")]
+            //[OffsetCN("Search 48 8D 05 ? ? ? ? 48 8D 4B ? 48 89 03 E8 ? ? ? ? 48 8D 4B ? E8 ? ? ? ? 33 C9 Add 3 TraceRelative")]
             internal static IntPtr VTable;
             
             [Offset("Search 38 9F ? ? ? ? 48 8D 8D ? ? ? ? Add 2 Read32")]
